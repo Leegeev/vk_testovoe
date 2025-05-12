a8ea78797084b3433e4dea68e2247bb12f450644
Leegee <leegee@MacBook-Pro-Leegee.local>
2025-05-12 21:45:00 +0300

    server is shutting down carefully

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,7 +16,7 @@ import (
 )
 
 func main() {
-	// 1) поднимаем TCP-лисенер на 50051
+	// 1) стартуем сервер
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatal(status.Errorf(
@@ -26,20 +26,42 @@ func main() {
 		))
 	}
 
-	// 2) создаём gRPC-сервер
 	grpcServer := grpc.NewServer()
+	srv := NewServer()
+	pb.RegisterPubSubServer(grpcServer, srv)
 
-	// 3) регистрируем наш сервис
-	pb.RegisterPubSubServer(grpcServer, NewServer())
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatal(status.Errorf(
+				codes.Internal,
+				"failed to serve gRPC server: %v",
+				err,
+			))
+		}
+	}()
 
-	log.Println("gRPC server listening on :50051")
+	log.Println("Server started on: 50051")
 
-	// 4) стартуем
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatal(status.Errorf(
-			codes.Internal,
-			"failed to serve gRPC server: %v",
-			err,
-		))
+	// 2) ждём сигнал на shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+	log.Println("Shutdown signal received")
+
+	// 3) контекст с таймаутом
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// 4) сначала gRPC graceful stop
+	grpcServer.GracefulStop()
+	log.Println("gRPC server stopped")
+
+	// 5) потом шина событий
+	if err := srv.bus.Close(ctx); err != nil {
+		log.Printf("SubPub shutdown incomplete: %v", err)
+	} else {
+		log.Println("SubPub shutdown complete")
 	}
+
+	log.Println("All done, exiting")
 }
